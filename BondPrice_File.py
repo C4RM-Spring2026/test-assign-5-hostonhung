2c72854fbe2071eed5d9ddb3947ca553a1b58da0
hostonhung <hostonhung@hotmail.com>
2026-02-28 15:57:43 -0500

    Refactor getBondPrice for bond price calculation
    Refactor getBondPrice function to improve calculation method.

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,9 +1,16 @@
 import numpy as np
+
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    m_eff = m * ppy
-    coupon = face * couponRate / ppy
-    i = np.arange(1, m_eff + 1)
-    discount = 1 / (1 + y / ppy) ** i
-    price = np.sum(coupon * discount) + face * discount[-1]
-    return(price)
+    
+    n = m * ppy          
+    rate = y / ppy          
+    coupon = face * couponRate / ppy  
+    
+    i = np.arange(1, n + 1)           
+    df = (1 + rate) ** (-i)           
+
+    pvcf   = coupon * df                   
+    pvcf[-1] += face * df[-1]
+    
+    return np.sum(pvcf)   
   
