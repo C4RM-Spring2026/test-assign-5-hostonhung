542cc42b7b54cd3ab74bece45c78c4e78caf9e36
hostonhung <hostonhung@hotmail.com>
2026-02-28 16:18:22 -0500

    Add files via upload

---
+++ FizzBuzz_File.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def FizzBuzz(start, finish):
+
+    nums   = np.arange(start, finish + 1)
+    result = np.array(nums, dtype=object)
+
+    result[nums % 3 == 0] = 'fizz'
+    result[nums % 5 == 0] = 'buzz'
+    result[(nums % 3 == 0) & 
+           (nums % 5 == 0)] = 'fizzbuzz'
+
+    return result
