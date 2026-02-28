4729671d9c0f26d2d564aac2a12f0a5503cb316a
hostonhung <hostonhung@hotmail.com>
2026-02-28 15:18:48 -0500

    Add files via upload

---
+++ BondDuration_File.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def getBondPrice(y, face, couponRate, m, ppy=1):
+    
+    n = m * ppy          
+    rate = y / ppy          
+    coupon = face * couponRate / ppy  
+    
+    i = np.arange(1, n + 1)           
+    df = (1 + rate) ** (-i)           
+
+    pvcf   = coupon * df                   
+    pvcf[-1] += face * df[-1]
+    
+    return np.sum(pvcf)   
+    y = 0.03
+    face = 2000000
+    couponRate = 0.04
+    m = 10   
+
+    print(getBondPrice(y, face, couponRate, m))
